8d721d2e1550ce0d8cef5b2a839969d90407cf23
hr188 <himanshuray188@gmail.com>
2023-09-03 16:50:37 +0530

    added authentication using Clerk and added theme switch button using shad cn

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+const AuthLayout = ({children}: {children: React.ReactNode}) => {
+    return ( <div className="w-full h-full flex items-center content-center" >
+      
+        
+    </div> );
+}
+ 
+export default AuthLayout;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-
-export default function Home() {
-  return (
-    <>
-        <h1 className='text-3xl font-bold text-indigo-950' >Discord Clone</h1>
-    </>
-  )
-}
